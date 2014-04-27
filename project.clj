d656c2c2afb8443ff98a30af6708a927c3829a9f
Gordon Syme <gordonsyme@gmail.com>
2014-04-27 19:09:45 +0100

    Add dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "GNU General Public License Version 3"
             :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.typed "0.2.44"]
+                 [me.raynes/fs "1.4.4"]
+                 [amazonica "0.2.12"]]
   :main ^:skip-aot tardis-clj.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :test-selectors {:default (complement :integration)
+                   :integration :integration
+                   :all (fn [_] true)})

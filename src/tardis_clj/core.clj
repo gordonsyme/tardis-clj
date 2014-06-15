f7e4b23bfd3f13aa4a8ca982e7ea03cc063b5be9
Gordon Syme <gordonsyme@gmail.com>
2014-06-15 18:04:06 +0100

    Start adding restore functionality

--- src/tardis_clj/core.clj
+++ src/tardis_clj/core.clj
@@ -1,8 +1,47 @@
 (ns tardis-clj.core
-  (:require [tardis-clj.tree :as tree])
+  (:require [clojure.java.io :as io]
+            [tardis-clj.storage :as storage]
+            [tardis-clj.tree :as tree]
+            [tardis-clj.util :refer (inspect)])
+  (:import [java.io File])
   (:gen-class))
+
+(set! *warn-on-reflection* true)
+
+(defn skip
+  [^File file]
+  (re-find #"\/\.git(?:$|/)" (.getPath file)))
+
+(defn init
+  []
+  (storage/init (io/file (System/getenv "HOME") ".tardis")))
+
+(defn restorable-trees
+  [manifest from]
+  (:trees manifest)
+  (let [get-subtree (fn [tree] (get-in tree (fs/split from)))]
+    (->> manifest
+         :trees
+         (map :tree)
+         (map get-subtree))))
+
+;;(defn restore-tree
+;;  [tree dir]
+;;  {:pre [(fs/directory? dir)]}
+;;  (doseq [[path {:keys [key]}] tree]
+
+(defn leaf?
+  [node]
+  (and (:key node) (:metadata node)))
+
+(defn restore
+  [manifest from to]
+  (let [trees (map (partial tree/flatten-tree leaf?) (restorable-trees manifest from))]
+    trees))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [root-dir & args]
-  (println (pr-str (tree/build-manifest root-dir))))
+  [root-dir & dirs]
+  (let [backup-dirs (map io/file (inspect (cons root-dir dirs)))
+        manifest (tree/build-manifest skip backup-dirs)]
+    (println (pr-str manifest))))

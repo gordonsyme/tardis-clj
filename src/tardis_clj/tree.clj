c99ec5d3fef05f1a76d819fe2f03e87235c7a630
Gordon Syme <gordonsyme@gmail.com>
2014-07-16 16:06:31 +0100

    Add visit-tree.
    visit-tree calls a visit function for each leaf in the tree, passing the
    path to the leaf and the leaf data. It returns a pair of trees, the
    first containing all the successes, the second containing the failures.

--- src/tardis_clj/tree.clj
+++ src/tardis_clj/tree.clj
@@ -1,6 +1,7 @@
 (ns tardis-clj.tree
   (:require [clojure.java.io :as io]
             [clojure.edn :as edn]
+            [clojure.zip :as z]
             [me.raynes.fs :as fs]
             [clj-time.core :as time]
             [tardis-clj.nio :as nio]
@@ -81,3 +82,62 @@
 (defn flatten-tree
   [leaf? tree]
   (flatten-tree* leaf? [] tree))
+
+(defn tree-zipper*
+  [leaf? tree]
+  (let [inner-node? (fn [n] (and (map? n) (not (leaf? n))))
+        branch? (fn [n]
+                  (or (inner-node? n)
+                      (inner-node? (second n))))
+        children (fn [n]
+                  (seq (if (inner-node? n)
+                        n
+                        (second n))))
+        make-node (fn [n children]
+                    (throw (ex-info "unsupported operation" {})))]
+    (z/zipper branch? children make-node tree)))
+
+(defn tree-zipper
+  "A tree is a nested map, the first level can only have one key at this time.
+  E.g.
+  { \"/\" { \"home\" { \"alice\" {...}
+                       \"bob\" {...}}}}
+
+  Editing nodes is unsupported"
+  [leaf? tree]
+  {:pre [(= 1 (count tree))]}
+  (let [inner-node? (fn [n] (and (map? n) (not (leaf? n))))
+        branch? (fn [n] (inner-node? (second n)))
+        children (fn [n] (seq (second n)))
+        make-node (fn [n children]
+                    (throw (ex-info "unsupported operation" {})))]
+    (z/zipper branch? children make-node (first (seq tree)))))
+
+(defn- tree-zipper-path
+  "Get the path to the currently selected node in the tree, as a vector."
+  [tz]
+  (conj (->> tz z/path (map first) vec) (-> tz z/node first)))
+
+(defn- visit-tree*
+  [leaf? zipper visit-fn success failure]
+  (let [node-data (-> zipper z/node second)
+        path (tree-zipper-path zipper)
+        ;; Ewww... However this does do the right thing :(
+        [s f] (if (leaf? node-data)
+                (if (visit-fn path node-data)
+                  [(assoc-in success path node-data) failure]
+                  [success (assoc-in failure path node-data)])
+                [success failure])]
+    (if-not (z/end? zipper)
+      (recur leaf? (z/next zipper) visit-fn s f)
+      [s f])))
+
+(defn visit-tree
+  "Visit each leaf in tree. 'visit-fn' is a two-arg function that is passed the
+  path to the leaf (as a vector)
+  Returns a pair of trees, the first containing all the successes, the second
+  containing the failures."
+  [leaf? tree visit-fn]
+  (let [zipper (tree-zipper leaf? tree)]
+    (when-not (z/end? zipper)
+      (visit-tree* leaf? zipper visit-fn {} {}))))
